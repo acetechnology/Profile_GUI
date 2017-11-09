7c3a835779674c10eeac1a8c8e5bb3d72d2be344
acetechnology <amaechiemeka.o@gmail.com>
2017-11-09 01:42:08 +0100

    landscape added

--- app/src/main/java/biz/acetechnology/profile/MainActivity.kt
+++ app/src/main/java/biz/acetechnology/profile/MainActivity.kt
@@ -1,12 +1,20 @@
 package biz.acetechnology.profile
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+        logo.setImageDrawable(rounded)
     }
 }
